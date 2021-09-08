2b2043101335be45113e48a5824125ae41fe2ac9
Abhishek <abhishekb042@gmail.com>
2021-09-08 23:16:41 +0530

    This is a code for single  player at staring position

---
+++ src/com/bridgelabz/SnakeAndLadderUC1.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class SnakeAndLadderUC1 {
+    public static void main(String[] args) {
+        System.out.println("This is a Snake and Ladder Game");
+        int playerPosition = 0;
+        System.out.println("Player is at initial position " + playerPosition);
+    }
+}
